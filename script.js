cc5a7d882172c44194f06467d6f2a4431913a147
myFirstgitrepo <tanvirmahin607@gmail.com>
2024-08-13 16:45:51 +0600

    proprly animated destnation section

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ tl.from(".popular-section",{
 
     }
 })
+// destinationSection start//
 tl.from(".your-destination .destination-container",{
     duration:0.8,
     opacity:0,
@@ -26,47 +27,109 @@ tl.from(".your-destination .destination-container",{
 
     }
 })
-tl.from(".your-destination .row-1 .country-card",{
+tl.from(".your-destination .row-1 .country-card #img-1",{
     x:1200,
     duration:0.7,
     opacity:0,
     stagger:0.7,
     scrollTrigger: {
-        trigger:".your-destination .row-1 .country-card",
+        trigger:".your-destination .row-1 .country-card #img-1",
         scroller: "body",
         start:"top 40%",
         end:"top 60%",
         scrub:2
 
     }
+    
 })
-tl.from(".your-destination .row-2 .country-card",{
+tl.from(".your-destination .row-1 .country-card #img-2",{
+    x:1200,
+    duration:0.7,
+    opacity:0,
+    // stagger:0.7,
+    scrollTrigger: {
+        trigger:".your-destination .row-1 .country-card #img-2",
+        scroller: "body",
+        start:"top 30%",
+        end:"top 10%",
+        scrub:2
+
+    }
+    
+})
+tl.from(".your-destination .row-1 .country-card p",{
+    x:-1200,
+    duration:0.7,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".your-destination .row-1 .country-card p",
+        scroller: "body",
+        start:"top 54%",
+        end:"top 50%",
+        scrub:2
+
+    }
+    
+})
+tl.from(".your-destination .row-2 .country-card img",{
     x:-1200,
     duration:0.7,
     opacity:0,
     stagger:0.7,
     scrollTrigger: {
-        trigger:".your-destination .row-2 .country-card",
+        trigger:".your-destination .row-2 .country-card img",
         scroller: "body",
-        start:"top 40%",
-        end:"top 60%",
+        start:"top 30%",
+        end:"top 17%",
+        // markers:true,
         scrub:2
 
     }
 })
-tl.from(".your-destination .row-3 .country-card",{
+
+tl.from(".your-destination .row-2 .country-card p",{
+    // x:1200,
+    duration:0.7,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".your-destination .row-2 .country-card p",
+        scroller: "body",
+        start:"top 30%",
+        end:"top 25%",
+        markers:true,
+        scrub:2
+
+    }
+    
+})
+tl.from(".your-destination .row-3 .country-card img",{
     x:-1200,
     duration:0.7,
     opacity:0,
     stagger:0.7,
     scrollTrigger: {
-        trigger:".your-destination .row-3 .country-card",
+        trigger:".your-destination .row-3 .country-card img",
         scroller: "body",
-        start:"top 40%",
+        start:"top 20%",
         end:"top 60%",
+        // markers:true,
         scrub:2
 
     }
+})
+tl.from(".your-destination .row-3 .country-card p",{
+    duration:0.7,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".your-destination .row-3 .country-card p",
+        scroller: "body",
+        start:"top 47%",
+        end:"top 60%",
+        // markers:true,
+        scrub:2
+
+    }
+    
 })
 tl.from(".choose .Chose-container",{
     y:-240,
@@ -76,7 +139,7 @@ tl.from(".choose .Chose-container",{
     scrollTrigger: {
         trigger:".choose .Chose-container",
         scroller: "body",
-        start:"top 40%",
+        start:"top 20%",
         end:"top 20%",
         scrub:2
 
